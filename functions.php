9da6d5914272a27b8a87fa94b29b499b8ce90c5c
Aldo Paz Velasquez <aldveq93@Aldos-Mac-mini.local>
2022-11-23 20:44:21 -0600

    added OOP approach to functions.php

--- functions.php
+++ functions.php
@@ -13,141 +13,14 @@ if ( ! defined( '_S_VERSION' ) ) {
 }
 
 /**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
+ * Autoloaders.
  */
-function fox_university_setup() {
-	/*
-		* Make theme available for translation.
-		* Translations can be filed in the /languages/ directory.
-		* If you're building a theme based on Fox University, use a find and replace
-		* to change 'fox-university' to the name of your theme in all the template files.
-		*/
-	load_theme_textdomain( 'fox-university', get_template_directory() . '/languages' );
+require get_template_directory() . '/inc/helpers/autoloader.php';
 
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
-
-	/*
-		* Let WordPress manage the document title.
-		* By adding theme support, we declare that this theme does not use a
-		* hard-coded <title> tag in the document head, and expect WordPress to
-		* provide it for us.
-		*/
-	add_theme_support( 'title-tag' );
-
-	/*
-		* Enable support for Post Thumbnails on posts and pages.
-		*
-		* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		*/
-	add_theme_support( 'post-thumbnails' );
-
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus(
-		array(
-			'menu-1' => esc_html__( 'Primary', 'fox-university' ),
-		)
-	);
-
-	/*
-		* Switch default core markup for search form, comment form, and comments
-		* to output valid HTML5.
-		*/
-	add_theme_support(
-		'html5',
-		array(
-			'search-form',
-			'comment-form',
-			'comment-list',
-			'gallery',
-			'caption',
-			'style',
-			'script',
-		)
-	);
-
-	// Set up the WordPress core custom background feature.
-	add_theme_support(
-		'custom-background',
-		apply_filters(
-			'fox_university_custom_background_args',
-			array(
-				'default-color' => 'ffffff',
-				'default-image' => '',
-			)
-		)
-	);
-
-	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
-
-	/**
-	 * Add support for core custom logo.
-	 *
-	 * @link https://codex.wordpress.org/Theme_Logo
-	 */
-	add_theme_support(
-		'custom-logo',
-		array(
-			'height'      => 250,
-			'width'       => 250,
-			'flex-width'  => true,
-			'flex-height' => true,
-		)
-	);
+function fox_university_get_theme_instance() {
+	\FOX_UNIVERSITY_THEME\Inc\FOX_UNIVERSITY_THEME::get_instance();
 }
-add_action( 'after_setup_theme', 'fox_university_setup' );
-
-/**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- */
-function fox_university_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'fox_university_content_width', 640 );
-}
-add_action( 'after_setup_theme', 'fox_university_content_width', 0 );
-
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function fox_university_widgets_init() {
-	register_sidebar(
-		array(
-			'name'          => esc_html__( 'Sidebar', 'fox-university' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'fox-university' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
-}
-add_action( 'widgets_init', 'fox_university_widgets_init' );
-
-/**
- * Enqueue scripts and styles.
- */
-function fox_university_scripts() {
-	wp_enqueue_style( 'fox-university-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'fox-university-style', 'rtl', 'replace' );
-
-	wp_enqueue_script( 'fox-university-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
-}
-add_action( 'wp_enqueue_scripts', 'fox_university_scripts' );
+fox_university_get_theme_instance();
 
 /**
  * Implement the Custom Header feature.
---
+++ inc/classes/assets.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+* @package Fox_University
+*/
+
+namespace FOX_UNIVERSITY_THEME\Inc;
+
+use FOX_UNIVERSITY_THEME\Inc\Traits\Singleton;
+
+class ASSETS {
+	use Singleton;
+
+	protected function __construct() {
+		// Actions & Filters
+		$this->setup_hooks();
+	}
+
+	protected function setup_hooks() {
+		add_action( 'wp_enqueue_scripts', [$this, 'fox_university_scripts'] );
+	}
+
+	/**
+	* Enqueue scripts and styles.
+	*/
+	public function fox_university_scripts() {
+		wp_enqueue_style( 'fox-university-style', get_stylesheet_uri(), array(), _S_VERSION );
+		wp_style_add_data( 'fox-university-style', 'rtl', 'replace' );
+
+		wp_enqueue_script( 'fox-university-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+
+		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+			wp_enqueue_script( 'comment-reply' );
+		}
+	}
+}
